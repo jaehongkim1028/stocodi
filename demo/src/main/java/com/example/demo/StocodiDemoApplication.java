7b98de7cd1b4c74a2570651da63dea254ed7823f
Sunghan Shin <crossrunway01@gmail.com>
2023-05-17 21:28:56 +0900

    feat(IMG_5843.mp4) : /resources/static/video/IMG_5843.mp4 경로 샘플  비디오 파일 추가

--- demo/src/main/java/com/example/demo/StocodiDemoApplication.java
+++ demo/src/main/java/com/example/demo/StocodiDemoApplication.java
@@ -2,10 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class StocodiDemoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(StocodiDemoApplication.class, args);
 	}
