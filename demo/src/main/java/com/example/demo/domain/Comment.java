b7bff65b5089d01ae2e84fd97f4111d3a973ad23
F-hiller <jjun6912@gmail.com>
2023-05-18 01:15:22 +0900

    feat(domain) : Comment 엔티티 추가.

---
+++ demo/src/main/java/com/example/demo/domain/Comment.java
@@ -0,0 +1,35 @@
+package com.example.demo.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Comment extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long commentId;
+
+    @Column(name = "EMAIL")
+    private String email;
+
+    @Column(name = "CONTENT_ID")
+    private Long contentId;
+
+    @Column(length = 500, nullable = false)
+    private String commentContent;
+
+    // Builder를 통한 초기화
+    @Builder
+    public Comment(Long commentId, String email, Long contentId, String commentContent) {
+        this.commentId = commentId;
+        this.contentId = contentId;
+        this.email = email;
+        this.commentContent = commentContent;
+    }
+}
