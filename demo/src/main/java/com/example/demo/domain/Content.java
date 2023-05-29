fc3cf13f32e3ae0b6dff29bdd50969aa9e6d5dba
Sunghan Shin <crossrunway01@gmail.com>
2023-05-29 16:40:33 +0900

    feat(Content): /컨텐츠 개시를 위한 컬럼추가 (hashtag, videoID, writer)

--- demo/src/main/java/com/example/demo/domain/Content.java
+++ demo/src/main/java/com/example/demo/domain/Content.java
@@ -1,6 +1,63 @@
 package com.example.demo.domain;
 
-public class Content {
-    public String UserID;
-    public String ContentAddress;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Set;
+import java.util.HashSet;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Content extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "CONTENT_ID")
+    private Long contentId;
+
+    @Column(name = "EMAIL", length = 500, nullable = false)
+    private String email;
+
+    @ElementCollection
+    @CollectionTable(name = "HashTags", joinColumns = @JoinColumn(name = "CONTENT_ID"))
+    @Column(name = "hashtag")
+    private Set<String> hashtags = new HashSet<>();
+
+    @Column(name = "videoID")
+    private String videoID;
+
+    @Column(name = "writer")
+    private String writer;
+
+    @Column(length = 32, nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private Integer likeCount;
+
+    @Column(nullable = false)
+    private Integer scrapCount;
+
+    @Column(length = 500, nullable = false)
+    private String storePlace;
+
+    // Builder를 통한 초기화
+    @Builder
+    public Content(String email, Set<String> hashtags, String videoID, String writer, String title, Integer likeCount, Integer scrapCount, String storePlace) {
+        this.email = email;
+        this.hashtags = hashtags;
+        this.videoID = videoID;
+        this.writer = writer;
+        this.title = title;
+        this.likeCount = likeCount;
+        this.scrapCount = scrapCount;
+        this.storePlace = storePlace;
+    }
+
+    // update를 통한 수정
+    public void update() {
+    }
+
 }
