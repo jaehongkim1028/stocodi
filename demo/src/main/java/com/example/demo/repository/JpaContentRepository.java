d3f32a6c356a3a43d688347641ceaf22b246d1f1
Sunghan Shin <91328924+Hollyys@users.noreply.github.com>
2023-06-15 00:11:06 +0900

    feat(JPAContentRepository): content 기능추가

---
+++ demo/src/main/java/com/example/demo/repository/JpaContentRepository.java
@@ -0,0 +1,43 @@
+package com.example.demo.repository;
+
+import com.example.demo.domain.Content;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class JpaContentRepository implements ContentRepository{
+    private final EntityManager em;
+
+    public JpaContentRepository(EntityManager em){
+        this.em = em;
+    }
+
+    @Override
+    public Content save(Content content){
+        em.persist(content);
+
+        return content;
+    }
+
+    @Override
+    public Optional<Content> findByContentId(Long contentId){
+        Content content = em.find(Content.class, contentId);
+        return Optional.ofNullable(content);
+    }
+
+    @Override
+    public List<Content> findAll() {
+        return em.createQuery("select a from Content a", Content.class)
+                .getResultList();
+    }
+
+    @Override
+    public void delete(Long contentId){
+        Content content = findByContentId(contentId).orElseThrow(()
+                -> new EntityNotFoundException("Content not found"));
+    }
+}
