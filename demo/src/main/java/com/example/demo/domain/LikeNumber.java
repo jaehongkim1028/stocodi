b66e341139617dda35f0c571a753b21d78d34321
Sunghan Shin <91328924+Hollyys@users.noreply.github.com>
2023-06-15 00:03:00 +0900

    fix(LikeNumber): LikeNumber domain 수정

--- demo/src/main/java/com/example/demo/domain/LikeNumber.java
+++ demo/src/main/java/com/example/demo/domain/LikeNumber.java
@@ -1,27 +1,41 @@
-
 package com.example.demo.domain;
-
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
-@Getter
-@NoArgsConstructor
 @Entity
-public class LikeNumber extends BaseTimeEntity {
+public class LikeNumber {
     @Id
+    @Column(name = "LIKENUMBER_ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long likeNumberId;
+
     @Column(name = "EMAIL")
     private String email;
 
     @Column(name = "CONTENT_ID")
     private Long contentId;
 
-    // Builder를 통한 초기화
-    @Builder
-    public LikeNumber(String email, Long contentId) {
-        this.contentId = contentId;
+    public long getLikeNumberId() {
+        return likeNumberId;
+    }
+
+    public void setLikeNumberId(long likeNumberId) {
+        this.likeNumberId = likeNumberId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
         this.email = email;
+    }
+
+    public Long getContentId() {
+        return contentId;
+    }
+
+    public void setContentId(Long contentId) {
+        this.contentId = contentId;
     }
 }
